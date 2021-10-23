a3768468d82bea427199e8766dce5d5be2a054dc
kerry-davis <pulsta@gmail.com>
2021-10-24 09:52:43 +1300

    more news sources added

--- index.js
+++ index.js
@@ -8,20 +8,96 @@ const app = express()
 
 const newspapers = [
   {
-    name: 'thetimes',
-    address: 'https://www.thetimes.co.uk/environment/climate-change',
-    base: ''
+      name: 'cityam',
+      address: 'https://www.cityam.com/london-must-become-a-world-leader-on-climate-change-action/',
+      base: ''
   },
   {
-    name: 'guardian',
-    address: 'https://www.theguardian.com/environment/climate-crisis',
-    base: ''
+      name: 'thetimes',
+      address: 'https://www.thetimes.co.uk/environment/climate-change',
+      base: ''
   },
   {
-    name: 'telegraph',
-    address: 'https://www.telegraph.co.uk/climate-change',
-    base: 'https://www.telegraph.co.uk'
-  }
+      name: 'guardian',
+      address: 'https://www.theguardian.com/environment/climate-crisis',
+      base: '',
+  },
+  {
+      name: 'telegraph',
+      address: 'https://www.telegraph.co.uk/climate-change',
+      base: 'https://www.telegraph.co.uk',
+  },
+  {
+      name: 'nyt',
+      address: 'https://www.nytimes.com/international/section/climate',
+      base: '',
+  },
+  {
+      name: 'latimes',
+      address: 'https://www.latimes.com/environment',
+      base: '',
+  },
+  {
+      name: 'smh',
+      address: 'https://www.smh.com.au/environment/climate-change',
+      base: 'https://www.smh.com.au',
+  },
+  {
+      name: 'un',
+      address: 'https://www.un.org/climatechange',
+      base: '',
+  },
+  {
+      name: 'bbc',
+      address: 'https://www.bbc.co.uk/news/science_and_environment',
+      base: 'https://www.bbc.co.uk',
+  },
+  {
+      name: 'es',
+      address: 'https://www.standard.co.uk/topic/climate-change',
+      base: 'https://www.standard.co.uk'
+  },
+  {
+      name: 'sun',
+      address: 'https://www.thesun.co.uk/topic/climate-change-environment/',
+      base: ''
+  },
+  {
+      name: 'dm',
+      address: 'https://www.dailymail.co.uk/news/climate_change_global_warming/index.html',
+      base: ''
+  },
+  {
+      name: 'nyp',
+      address: 'https://nypost.com/tag/climate-change/',
+      base: ''
+  },
+  {
+    name: 'stuff',
+    address: 'https://www.stuff.co.nz/environment/climate-news/',
+    base: 'https://www.stuff.co.nz'
+  },
+  {
+    name: 'rnz',
+    address: 'https://www.rnz.co.nz/tags/climate/',
+    base: 'https://www.rnz.co.nz'
+  },
+  {
+    name: 'nzh',
+    address: 'https://www.nzherald.co.nz/topic/climate-change/',
+    base: 'https://www.nzherald.co.nz'
+  },
+  // {
+  //   name: '1news',
+  //   address: 'https://www.1news.co.nz/tags/environment/',
+  //   base: ''
+  // },
+  {
+    name: 'newshub',
+    address: 'https://www.newshub.co.nz/home/world/environment.html',
+    base: ''
+  }    
+  
 ]
 
 const articles = []
