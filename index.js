7a106bb336e4353bd579accff4cf57949010a16f
kerry-davis <pulsta@gmail.com>
2021-10-24 09:02:45 +1300

    default get req on /

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ const { response } = require('express')
 
 const app = express()
 
-
+app.get('/', (req, res) => {
+  res.json('Welcome to my  Climate Change News API')
+})
 
 
 app.listen(PORT, () => console.log(`server running on PORT ${PORT}`))
