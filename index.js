9d9667909019df1a1c9e27ce2453a043ab93fc13
kerry-davis <pulsta@gmail.com>
2021-10-24 09:25:37 +1300

    changed logic to get articles from an array of newspapers on app load

--- index.js
+++ index.js
@@ -5,13 +5,57 @@ const cheerio = require('cheerio')
 const { response } = require('express')
 
 const app = express()
+
+const newspapers = [
+  {
+    name: 'thetimes',
+    address: 'https://www.thetimes.co.uk/environment/climate-change',
+    base: ''
+  },
+  {
+    name: 'guardian',
+    address: 'https://www.theguardian.com/environment/climate-crisis',
+    base: ''
+  },
+  {
+    name: 'telegraph',
+    address: 'https://www.telegraph.co.uk/climate-change',
+    base: 'https://www.telegraph.co.uk'
+  }
+]
+
 const articles = []
+const articlesTest = []
+
+newspapers.map(newspaper => {
+  axios.get(newspaper.address)
+    .then((response) => {
+      const html = response.data
+      const $ = cheerio.load(html)
+
+      $('a:contains("climate")', html).each(function () {
+        const title = $(this).text()
+        const url = $(this).attr('href')
+
+        articles.push({
+          title,
+          url: newspaper.base + url,
+          source: newspaper.name
+        })
+      })
+    })
+
+})
 
 app.get('/', (req, res) => {
   res.json('Welcome to my  Climate Change News API')
 })
 
 app.get('/news', (req, res) => {
+  res.json(articles)
+})
+
+app.get('/newstest', (req, res) => {
   axios.get('https://www.theguardian.com/environment/climate-crisis')
     .then((response) => {
       const html = response.data
@@ -20,12 +64,12 @@ app.get('/news', (req, res) => {
         const title = $(this).text()
         const url = $(this).attr('href')
 
-        articles.push({
+        articlesTest.push({
           title,
           url
         })
       })
-      res.json(articles)
+      res.json(articlesTest)
     }).catch(err => console.log(err))
 })
 
